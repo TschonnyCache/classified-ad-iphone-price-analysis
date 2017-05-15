6bb052c09b259a7b5432c0965db3d44f13dd040f
Jonas Kreusch <krejon@gmail.com>
2017-05-15 12:53:35 +0200

    work on initial parse. realized that vek xlsx does not contain zipcode but regionalcode started work on mapping zipcode to regionalode

--- storeInit.py
+++ storeInit.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
 from pyexcel_xlsx import get_data
-from rdflib import Graph, BNode, URIRef, Literal, RDF
+from rdflib import Graph, BNode, URIRef, Literal, RDF, RDFS, XSD, Namespace
 
+ns = Namespace("sw-kreusch")
 g = Graph()
 typePLZ = URIRef("http://dbpedia.org/ontology/zipCode")
-
+typeCounty = URIRef("http://dbpedia.org/ontology/county")
+hasVEK = URIRef("hasVEK")
+g.add( (hasVEK,RDF.type,RDF.Property ) )
+g.add( (hasVEK,RDFS.range,XSD.onNegativeInteger) )
 
 data = get_data("VGR_KreisergebnisseBand3.xlsx")
 vek = data["VEK je Einwohner"]
-for zipCodeArea in vek:
-    # there are some descriptive entries, that we can filter out easily
-    if len(zipCodeArea) > 5:
-        if zipCodeArea[6] == '3':
-            #print 'Postleitzahlbereich ' + str(zipCodeArea[2]) + ' Verfügbaren Einkommen pro Einwohner und Jahr in € ' + str(zipCodeArea[27]) + '\n'
-            plz = BNode(value=str(zipCodeArea[2]))
-            g.add( (plz, RDF.type, typePLZ) )
+# for countyEntry in vek:
+#     # there are some descriptive entries, that we can filter out easily
+#     if len(countyEntry) > 5:
+#         if countyEntry[6] == '3':
+#             if len(str(countyEntry[2])) == 5:
+#                 #print 'Postleitzahlbereich ' + str(countyEntry[2]) + ' Verfügbaren Einkommen pro Einwohner und Jahr in € ' + str(countyEntry[27]) + '\n'
+#                 county = URIRef(str(countyEntry[2]))
+#                 g.add( (county, RDF.type, typeCounty) )
+#                 g.add((county, hasVEK, Literal(countyEntry[27])))
 
-print g.serialize(format='turtle')
+#print g.serialize(format='turtle')
+
+data = get_data("AuszugGV1QAktuell.xlsx")
+counties = data["Onlineprodukt_Gemeinden_310317"]
+for community in counties:
+    if len(community) > 5:
+        #error parsing "Regionalcode" cus its unicode
+        regCode = str(community[2])+str(community[3])+str(community[4])
+        if community[14] is not None:
+            print regCode + " " + community
